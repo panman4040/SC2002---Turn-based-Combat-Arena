def5e050b4ce4f92d58eeda0e8de42bd8403a359
C250229 <C250229@e.ntu.edu.sg>
2026-04-05 17:17:18 +0800

    CLIGameUI skeleton which aligns with BattleEngine UI Placeholders

--- arena/ui/CLIGameUI.java
+++ arena/ui/CLIGameUI.java
@@ -1,20 +1,27 @@
 package arena.ui;
 import java.util.List;
 import java.util.Scanner;
-import arena.domain.entity.Combatant;
-public class CLIGameUI {
+import arena.domain.entity.Combatant; //waiting implementation
+import arena.domain.entity.Player; // waiting implementation
+import arena.domain.action.Action;
+import arena.engine.BattleContext;
+import arena.engine.Level; // waiting implementation
+
+
+public class CLIGameUI implements GameUI {
 
     private Scanner scanner;
-
     public CLIGameUI() {
         scanner = new Scanner(System.in);
     }
+
     public void displayMessage(String message) {
         System.out.println(message);
     }
 
     // Match BattleEngine from here
     // For (runBattle)
+
     public void displayStartingInfo() {
         System.out.println("=== Battle Start ===");
     }
@@ -24,12 +31,21 @@ public class CLIGameUI {
         System.out.println("Backup enemies have appeared!");
     }
 
-    //  Round number (runRound)
+    //  Round number for (runRound)
     public void displayRoundNumber(int roundNumber) {
         System.out.println("\n=== Round " + roundNumber + " ===");
     }
 
-    //  Turn order (runRound)
+    // Battle State for (runRound)
+    public void displayBattleState(BattleContext context){
+        System.out.println("=== Battle State ===");
+
+        //player info - Name, HP, MaxHp to be included soon
+
+        //enemies info - Name,Hp, MaxHp to be included soon
+    }
+
+    //  Turn order for (runRound)
     public void displayTurnOrder(List<Combatant> combatants) {
         System.out.print("Turn Order: ");
         for (int i = 0; i < combatants.size(); i++) {
@@ -59,20 +75,23 @@ public class CLIGameUI {
 
     // User Input
     // Difficulty selection
-    public int chooseDifficulty() {
+    public Level chooseDifficulty() {
         System.out.println("Choose difficulty:");
         System.out.println("1. Easy");
         System.out.println("2. Medium");
         System.out.println("3. Hard");
-        return scanner.nextInt();
+        int choice = scanner.nextInt();
+        return null; // placeholder for now
     }
     // Player action selection
-    public int getPlayerAction() {
+    public Action getPlayerAction(Player player, BattleContext context) {
         System.out.println("Choose action:");
         System.out.println("1. Basic Attack");
         System.out.println("2. Defend");
         System.out.println("3. Use Item");
         System.out.println("4. Special Skill");
-        return scanner.nextInt();
+
+        int choice = scanner.nextInt();
+        return null; //placeholder for now
     }
 }
