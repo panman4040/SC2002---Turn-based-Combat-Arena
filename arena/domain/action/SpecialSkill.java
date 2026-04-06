42b39da0e0040e175c3b63c29bd5daa7cfe24771
unknown <ngko0014@e.ntu.edu.sg>
2026-04-06 22:09:36 +0800

    changed special skill

--- arena/domain/action/SpecialSkill.java
+++ arena/domain/action/SpecialSkill.java
@@ -1,22 +1,17 @@
 package arena.domain.action;
 
-import arena.domain.entity.Combatant;
-import arena.engine.BattleContext;
-
 public abstract class SpecialSkill implements Action {
-    protected int cooldown = 0;
-    protected final int MAX_COOLDOWN = 3;
 
-    public boolean isAvailable() {
-        return cooldown == 0;
+    private final String name;
+    private final int cooldownDuration;
+
+    protected SpecialSkill(String name, int cooldownDuration) {
+        this.name             = name;
+        this.cooldownDuration = cooldownDuration;
     }
 
-    public void triggerCooldown() {
-        cooldown = MAX_COOLDOWN;
-    }
-
-    public void reduceCooldown() {
-        if (cooldown > 0)
-            cooldown--;
+    @Override
+    public String getName() {
+        return name;
     }
 }
