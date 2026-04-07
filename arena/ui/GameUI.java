f5a524122082453d69ab4d6442a3a5496183cf54
Nguyen Quang An <ngquangan2412@gmail.com>
2026-04-07 10:32:47 +0800

    Add displayTurnOrder() in GameUI interface

--- arena/ui/GameUI.java
+++ arena/ui/GameUI.java
@@ -1,13 +1,16 @@
 package arena.ui;
 
-import arena.domain.entity.Player;
 import arena.domain.action.Action;
+import arena.domain.entity.Combatant;
+import arena.domain.entity.Player;
 import arena.engine.BattleContext;
 import arena.engine.Level;
+import java.util.List;
 
 public interface GameUI {
     void displayMessage(String message);
     void displayBattleState(BattleContext context);
+    void displayTurnOrder(List<Combatant> combatants);
     Action getPlayerAction(Player player, BattleContext context);
     Level chooseDifficulty();
 }
